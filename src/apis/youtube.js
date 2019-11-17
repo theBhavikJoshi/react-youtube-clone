114c3bd3d98ae8d6ac138cd8c25d18d54ad8c180
BhavikJoshi <bhavik@getwalnut.com>
2019-11-17 15:14:15 +0530

    Youtube Clone React - Fixed Keys

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY  = 'AIzaSyAldQ9u209cCWSMPZmONED5EiRZwYGNURc';
+import { KEY } from './../keys';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
